e5731b49c496a174f88c6897bf2a017698bb9a4a
yumemi-shito <sh_ito@yumemi.co.jp>
2020-01-08 14:52:39 +0900

    Initial Commit

---
+++ TestPJ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPJ
+//
+//  Created by 伊藤 翔太 on 2020/01/08.
+//  Copyright © 2020 伊藤 翔太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestPJTests/TestPJTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestPJTests.swift
+//  TestPJTests
+//
+//  Created by 伊藤 翔太 on 2020/01/08.
+//  Copyright © 2020 伊藤 翔太. All rights reserved.
+//
+
+import XCTest
+@testable import TestPJ
+
+class TestPJTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
